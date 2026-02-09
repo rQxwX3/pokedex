24d0f4a997f343046049c09d11b926a3967794a0
rQxwX3 <github@scrt.simplelogin.com>
2026-02-09 21:47:20 +0300

    Implement CleanInput()

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
+
+func CleanInput(text string) []string {
+	words := strings.Fields(text)
+
+	return words
+}
 
 func main() {
 	fmt.Println("Hello, World!")
 }
-
