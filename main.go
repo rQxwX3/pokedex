52665eb92446d0834612a9f40b11a7b5e739a624
rQxwX3 <github@scrt.simplelogin.com>
2026-02-09 23:12:44 +0300

    Define basic REPL logic

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func CleanInput(text string) []string {
 	words := strings.Fields(text)
 
+	for i := 0; i < len(words); i++ {
+		words[i] = strings.ToLower(words[i])
+	}
+
 	return words
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		words := CleanInput(input)
+		fmt.Println("Your command was:", words[0])
+	}
 }
